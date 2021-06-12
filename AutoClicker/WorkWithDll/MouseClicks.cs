3afe8c0379177b98b3edf3bbbfc537d52ee19859
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2021-06-12 03:49:58 +0300

    Added demo version

---
+++ AutoClicker/WorkWithDll/MouseClicks.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoClicker.WorkWithDll
+{
+    public class MouseClicks
+    {
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LeftDown = 0x00000002,
+            LeftUp = 0x00000004,
+            MiddleDown = 0x00000020,
+            MiddleUp = 0x00000040,
+            RightDown = 0x00000008,
+            RightUp = 0x00000010
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(out MousePoint lpMousePoint);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        public static void SetCursorPosition(int x, int y)
+        {
+            SetCursorPos(x, y);
+        }
+        public static void SetCursorPosition(MousePoint point)
+        {
+            SetCursorPos(point.X, point.Y);
+        }
+
+        public static MousePoint GetCursorPosition()
+        {
+            var gotPoint = GetCursorPos(out MousePoint currentMousePoint);
+            if (!gotPoint) 
+            { 
+                currentMousePoint = new MousePoint(0, 0); 
+            }
+            return currentMousePoint;
+        }
+
+        public static void MouseEvent(MouseEventFlags value)
+        {
+            MousePoint pos = GetCursorPosition();
+            mouse_event((int)value, pos.X, pos.Y, 0, 0);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MousePoint
+        {
+            public int X;
+            public int Y;
+            public MousePoint(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
